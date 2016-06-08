7358f1e4d555f0fac5b4f41ec2cb63d51cd7e96c
shihan.chen <shihan.chen@tentcoo.com>
2016-06-08 17:41:58 +0800

    1

--- CT4FMDBDemo/ViewController.m
+++ CT4FMDBDemo/ViewController.m
@@ -27,13 +27,13 @@
         [model saveOrUpdate];
     }
         //具体请看ChanModel类中方法
+        //分页
         NSArray *a = [ChanModel findByCriteria:@"WHERE sex=1" Page:1 PageSize:3];
        NSArray * arr =[ChanModel findAll];
         for (ChanModel *model in arr) {
             NSLog(@"%@",model.description);
         }
         NSLog(@"%ld",arr.count);
-        
     });
     
     
